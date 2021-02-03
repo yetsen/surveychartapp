b984f8f9609e3723238e436e46bc957e40353714
Yunus Emre Tekin <yunusemretekin1@gmail.com>
2021-02-03 16:35:05 +0300

    save & clear button left aligned

--- src/main/webapp/app/core/survey/survey-card.vue
+++ src/main/webapp/app/core/survey/survey-card.vue
@@ -7,9 +7,9 @@
                 </div>
             </div>
             <survey :survey='survey'></survey>
-            <div v-if="!isCompletionPage" class="panel-footer card-footer text-center">
+            <div v-if="!isCompletionPage" class="panel-footer card-footer text-center" style="display: flow-root">
                 <input v-if="!survey.isFirstPage" type="button" @click="survey.prevPage()" value="Previous" class="btn sv_prev_btn float-left" >
-                <input type="button" @click="prepareClearAndExitModal()" value="Clear & Exit" class="btn sv_clear_btn" >
+                <input type="button" @click="prepareClearAndExitModal()" value="Clear & Exit" class="btn sv_clear_btn float-left" >
                 <input v-if="!survey.isLastPage" type="button" @click="survey.nextPage()" value="Next" class="btn sv_next_btn float-right">
                 <input v-if="survey.isLastPage" type="button" @click="survey.completeLastPage()" value="Complete" class="btn sv_complete_btn float-right" >
             </div>
