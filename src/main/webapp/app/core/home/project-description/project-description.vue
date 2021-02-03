e42e814b5296ed2e5c79778a71b5a712fc599045
Yunus Emre Tekin <yunusemretekin1@gmail.com>
2021-02-03 19:08:49 +0300

    ui improvements

--- src/main/webapp/app/core/home/project-description/project-description.vue
+++ src/main/webapp/app/core/home/project-description/project-description.vue
@@ -1,10 +1,11 @@
 <template>
     <header id="projectDescription" class="project-description">
         <div class="row h-100 align-items-center">
-            <div class="col-12 text-center">
-                <h1 class="font-weight-dark">Project Description</h1>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat, massa sit amet euismod ornare, purus dolor ornare massa, at scelerisque tortor orci sed mauris. Aenean hendrerit eget dolor eget elementum. Morbi in quam mi. Ut et risus ex. Nullam quis justo fringilla, dignissim sapien non, laoreet ligula. Suspendisse ac dolor rutrum, vulputate erat a, iaculis nunc. Nunc accumsan quam at ligula bibendum dapibus. Quisque eu purus cursus, tristique mi et, finibus ex. Morbi lobortis lobortis porttitor.</p>
+            <div class="col-4"></div>
+            <div class="col-4 text-center">
+                <h1 class="text-uppercase text-light font-weight-bold">Circular Economy Knowledge Hub</h1>
             </div>
+            <div class="col-4"></div>
         </div>
     </header>
 </template>
